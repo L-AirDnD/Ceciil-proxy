4b658d4c1fceb73ebe8d59817bcc37a7776393d8
Angela Lee <rubii.kun@gmail.com>
2019-02-20 10:20:45 -0800

    Refactor code to remove service copies

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../client')));
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
